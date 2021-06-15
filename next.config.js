fbbc0bc254a06b37d99bd5774cbf0e62305fc9fc
TemporalAgent7 <alejandro93@protonmail.com>
2021-06-15 09:46:38 -0700

    Update to next.js 11

--- next.config.js
+++
@@ -1,8 +0,0 @@
-const ghPages = process.env.DEPLOY_TARGET === 'gh-pages';
-
-module.exports = {
-  assetPrefix: ghPages ? '/temporalagent7.github.io/' : '', // customize this value if deployed to different URL
-  future: {
-    webpack5: true
-  }
-};
